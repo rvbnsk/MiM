a45306a17e1e25fa9fb5dfd3e88143a64a2c7a83
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2021-11-15 09:47:53 +0100

    Update zad_4_2.asm
    fix

--- lab_3/zad_4_2.asm
+++ lab_3/zad_4_2.asm
@@ -1,37 +1,33 @@
+MOV TMOD,#01000001b  ;8 bit counter + 16 bit timer
 wait:
-    JNB P3.0, start
-    JMP wait
+JNB P3.0, start
+MOV TL1, #00
+JMP wait
 
 start:
-    MOV P2, #0FFh
-    MOV TH0,#76
-    MOV TL0,#01
-    MOV TMOD,#01 ; 16-bitowy timer 0
-    SETB TR0 ; startujemy timer 0
-    JNB TF0,$
-    INC A
-    CLR TF0
-    JNB P3.0, start
-    MOV R3, A
-reset_diody:
-    MOV A, R3
-    MOV R0, A
-diody:
-    MOV TH0,#76
-    MOV TL0,#01
-    MOV TMOD,#01 ; 16-bitowy timer 0
-    SETB TR0 ; startujemy timer 0
-    JNB TF0,$
-    CLR TF0
-    DJNZ R0, diody
+MOV P2, #0FFh
+MOV TH0,#76
+MOV TL0,#01
+SETB TR0 ; startujemy timer 0
+JNB TF0,$
+INC A
+CLR TF0
+JNB P3.0, start
+CLR TR0
+MOV R3, A
+MOV A, R3
+MOV R0, A
 
-    MOV A, P2
-    CPL A
-    MOV B, #02
-    MUL AB
-    INC A
-    CPL A
-    MOV P2, A
-    JZ wait
-    JMP reset_diody
+diody:
+MOV TH0,#76
+MOV TL0,#01
+SETB TR0 ; start timer
+SETB TR1 ; start counter
+JNB TF0, $
+CLR TF0
+DJNZ R0, diody
+MOV A, TL1
+MOV P2, A
+CLR A
+JMP wait
 END
